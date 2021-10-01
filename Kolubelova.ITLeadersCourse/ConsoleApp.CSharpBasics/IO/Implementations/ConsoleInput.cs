a9ed7af493c0ec941d131ed88c8ece20643d25e4
Vera Kolubelova <vera.kolubelova@dataart.com>
2021-10-01 23:25:17 +0300

    Lesson Conditional statements

---
+++ Kolubelova.ITLeadersCourse/ConsoleApp.CSharpBasics/IO/Implementations/ConsoleInput.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.CSharpBasics.IO.Implementations
+{
+    using System;
+    using ConsoleApp.CSharpBasics.IO.Interfaces;
+
+    public class ConsoleInput : IInput
+    {
+        public string ReadLine()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
