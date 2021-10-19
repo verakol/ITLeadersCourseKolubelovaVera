e91bd742333d9bc791a937966fc8feaaa8b9adc9
Vera Kolubelova <vera.kolubelova@dataart.com>
2021-10-19 23:56:54 +0300

    Lesson7: Classes, Home Task

---
+++ Kolubelova.ITLeadersCourse/ConsoleApp.CSharpBasics/Classes/Person.cs
@@ -0,0 +1,52 @@
+﻿namespace ConsoleApp.CSharpBasics.Classes
+{
+    public class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public Person()
+        {
+            name = "Icognito";
+        }
+
+        public Person(string name)
+        {
+            this.name = name;
+        }
+
+        public string GetIntroduction()
+        {
+            return $"Hi! My name is {name}";
+        }
+    }
+
+    public struct Screen
+    {
+        public uint width;
+        public uint height;
+    }
+
+    public class TVSet
+    {
+        public Screen screen;
+
+        public TVSet(uint width, uint height)
+        {
+            screen = new Screen();
+            screen.width = width;
+            screen.height = height;
+        }
+
+        public string Display()
+        {
+            return "I display something!";
+        }
+    }
+
+}
---
+++ Kolubelova.ITLeadersCourse/ConsoleApp.CSharpBasics/Classes/PhoneNumbers.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp.CSharpBasics.Classes
+{
+    public class PhoneNumbers
+    {
+        private string number;
+
+        public PhoneNumbers(string number)
+        {
+            this.number = number;
+        }
+
+        public string GetPhoneNumber()
+        {
+            return $"My phone number is {number}";
+        }
+    }
+}
