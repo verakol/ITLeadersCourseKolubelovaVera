5cd4813ce9dd335792c13ec2d2dc0917021ce8af
Vera Kolubelova <vera.kolubelova@gmail.com>
2021-11-21 00:30:26 +0200

    Lesson9-HomeTask-done

--- Kolubelova.ITLeadersCourse/ConsoleApp.WebDriver/Pages/BasePage.cs
+++ Kolubelova.ITLeadersCourse/ConsoleApp.WebDriver/Pages/BasePage.cs
@@ -2,15 +2,26 @@
 {
     using ConsoleApp.WebDriver.Drivers;
     using OpenQA.Selenium;
+    using OpenQA.Selenium.Support.UI;
+    using System;
     using System.Collections.Generic;
 
     public class BasePage
     {
+        protected int DefaultTimeoutMilliseconds { get; } = 2000;
+
+        protected int DefaultTimeoutSeconds { get; set; } = 10;
         protected IWebDriver Driver { get; }
+
+        protected WebDriverWait WebDriverWait =>
+            new(Driver, TimeSpan.FromSeconds(DefaultTimeoutSeconds));
 
         public BasePage()
         {
             this.Driver = WebDriverManager.Driver;
+
+            // Implicit wait
+            //Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
         }
 
         protected void NavigateBack()
---
+++ Kolubelova.ITLeadersCourse/ConsoleApp.WebDriver/Pages/Lesson9WebDriverWait/WaitForMessagePage.cs
@@ -0,0 +1,46 @@
+﻿using OpenQA.Selenium;
+using ExpectedCondition = SeleniumExtras.WaitHelpers.ExpectedConditions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp.WebDriver.Pages.Lesson9WebDriverWait
+{
+    public class WaitForMessagePage : BasePage
+    {
+        private IWebElement LaunchCideExecutionButton => FindElement(By.Id("launchButton"));
+
+        private IWebElement TryToClickButton => FindElement(By.Id("messageButton"));
+
+        private By MessageLocator => By.Id("message");
+        private IWebElement MessageElement => FindElement(MessageLocator);
+
+        public WaitForMessagePage ClickOnLaunchButton()
+        {
+            LaunchCideExecutionButton.Click();
+
+            return this;
+        }
+
+        public WaitForMessagePage ClickOnTryToClickButton()
+        {
+            base.WebDriverWait.Until(ExpectedCondition.ElementToBeClickable(TryToClickButton));
+
+            TryToClickButton.Click();
+
+            return this;
+        }
+
+        public string GetMessage()
+        {
+            base.WebDriverWait.Until(ExpectedCondition.ElementIsVisible(MessageLocator));
+
+               string message = MessageElement.Text;
+
+            return message;
+        }
+
+    }
+}
